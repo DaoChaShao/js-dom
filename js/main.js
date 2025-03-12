b636d4627d17b890f810c30ef4a66c002dcf60f8
DaoChaShao <27882272@qq.com>
2025-03-13 00:09:41 +1300

    feat(main.js): enhance main functionality with new interactive features and image handling

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
 import {
-    randomInt, randomFloat, randomOption,
-    additionOld, additionNew,
-    welcomeLink, welcomeButton,
-    inserter, removeFirst, removeLast,
+    additionOld,
+    colourChange,
     copy,
-    replace, getLength,
-    randomColour, colourChange, moveIn, moveOut,
+    getLength,
+    imageChanger,
+    inserter,
+    moveIn,
+    moveOut,
     pressedKey,
+    randomColour,
+    randomFloat,
+    randomInt,
+    removeFirst,
+    removeLast,
+    replace,
+    timeShow,
+    welcomeButton,
+    welcomeLink,
 } from "./utils.js";
+
+const IMAGES = ["assets/img-cola.png", "assets/img-grape.png", "assets/img-orange.png", "assets/img-soda.png"]
 
 function main() {
     const a = parseFloat(randomFloat(1, 10).toFixed(2));
@@ -15,6 +27,9 @@ function main() {
     const result = parseFloat(additionOld(a, b).toFixed(2));
     const oMain = document.getElementById("addition");
     oMain.innerHTML = `${a} + ${b} = ${result}`;
+
+    const oBox = document.querySelector("#box");
+    oBox.innerText = "Container";
 
     const oWelcomeLink = document.querySelector("#welcome-link");
     oWelcomeLink.addEventListener("click", welcomeLink);
@@ -27,6 +42,7 @@ function main() {
     const oText = document.createTextNode("Hello, World!");
     oStrong.appendChild(oText);
     oTextDiv.appendChild(oStrong);
+    console.log(oTextDiv);
 
     const oList = document.querySelector("#list");
     const oInput = document.querySelector("#add-input");
@@ -110,6 +126,121 @@ function main() {
     const oAgentShow = document.createElement("span");
     oUser.appendChild(oAgentShow);
     oAgentShow.textContent = navigator.userAgent;
+
+
+    const oDivImg = document.querySelector("#image");
+    const oImgButton = document.createElement("button");
+    oImgButton.type = "button";
+    oImgButton.textContent = "Change Image";
+    oDivImg.appendChild(oImgButton);
+    const oImg = document.createElement("img");
+    oDivImg.appendChild(oImg);
+    oImgButton.addEventListener("click", _ => {
+        const {path, alt} = imageChanger(IMAGES);
+        oImg.src = path;
+        oImg.alt = alt;
+        oImg.title = `This is a ${alt}`;
+    })
+
+    const oClassButton = document.querySelector("#class-name-button");
+    const oClassShow = document.querySelector("#class-name-show");
+    oClassButton.addEventListener("click", _ => {
+        // oClassShow.className = "class-box";
+        oClassShow.classList.add("class-box");
+    })
+    const oClassDiv = document.querySelector("#class-name");
+    const oClassRemove = document.createElement("button")
+    oClassRemove.type = "button";
+    oClassRemove.textContent = "Remove Class";
+    oClassDiv.appendChild(oClassRemove);
+    oClassRemove.addEventListener("click", _ => {
+        oClassShow.classList.remove("class-box");
+    })
+    const oClassToggle = document.createElement("button");
+    oClassToggle.type = "button";
+    oClassToggle.textContent = "Toggle Class";
+    oClassDiv.appendChild(oClassToggle);
+    oClassToggle.addEventListener("click", _ => {
+        oClassShow.classList.toggle("class-box");
+    })
+
+    const oSliderImage = document.querySelector("#slider-image");
+    const oSliderContent = document.querySelector("#slider-content");
+    const {index, path, alt} = imageChanger(IMAGES);
+    oSliderImage.src = path;
+    oSliderImage.alt = alt;
+    oSliderContent.textContent = alt;
+    const oItem = document.querySelector(`.slider-list li:nth-child(${index + 1})`);
+    console.log(oItem);
+    oItem.classList.add("active");
+
+    const oHideInput = document.querySelector("#hide-input");
+    const oHideButton = document.querySelector("#hide-button");
+    oHideButton.addEventListener("click", _ => {
+        oHideInput.type = oHideInput.type === "password" ? "text" : "password";
+    })
+
+    const oCheckAll = document.querySelector("#check-all");
+    const oChecks = document.querySelectorAll("input[name='number']");
+    oCheckAll.addEventListener("change", () => {
+        oChecks.forEach(checkbox => {
+            checkbox.checked = oCheckAll.checked;
+        });
+    });
+
+    const oClickAll = document.querySelector("#click-all");
+    const oClicks = document.querySelectorAll("button[name='clicks']");
+    oClickAll.addEventListener("click", () => {
+        const isClicked = oClicks[0].disabled;
+        oClicks.forEach(button => {
+            button.disabled = !isClicked;
+        });
+    });
+
+    const oCusDataDivs = document.querySelectorAll("#cus-data div");
+    oCusDataDivs.forEach(div => {
+        console.log(div.dataset.id);
+    });
+
+    const oTimeDiv = document.querySelector("#time-now");
+    let timer = setInterval(function () {
+        const now = new Date();
+        oTimeDiv.textContent = timeShow(now);
+    }, 1000);
+    console.log(timer);
+
+    const oCountdownButton = document.querySelector("#countdown");
+    let seconds = 5;
+    timer = setInterval(function () {
+        seconds--;
+        oCountdownButton.textContent = `I am reading the agreement (${seconds})`;
+        if (seconds === 0) {
+            clearInterval(timer);
+            oCountdownButton.disabled = false;
+            oCountdownButton.textContent = "Agree";
+        }
+    }, 1000);
+
+    const oTimeImageDiv = document.querySelector("#slider-time-image");
+    const oTimeContent = document.querySelector("#slider-time-content");
+    let s = 0;
+    setInterval(function () {
+        s = (s + 1) % IMAGES.length;
+        if (s === IMAGES.length) {
+            s = 0;
+        }
+        console.log(s);
+        const {path, alt} = imageChanger(IMAGES);
+        const oItem = document.querySelector(`.slider-time-list li:nth-child(${s + 1})`);
+        oTimeImageDiv.src = path;
+        oTimeImageDiv.alt = alt;
+        oTimeImageDiv.title = `This is a ${alt}`;
+        oTimeContent.textContent = alt;
+        console.log(oItem)
+        const oItemActive = document.querySelector(".slider-time-list .time-active");
+        oItemActive.classList.remove("time-active");
+        oItem.classList.add("time-active");
+    }, 1000);
 }
 
 main()
